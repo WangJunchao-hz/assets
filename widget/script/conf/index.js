b15283162de70100cdc8bb80db156787e2fbe47f
zhugexiaoxiaoliang <1768368568@qq.com>
2017-06-01 10:58:21 +0800

    添加项目说明文件

--- widget/script/conf/index.js
+++ widget/script/conf/index.js
@@ -8,50 +8,19 @@
 define(function (require) {
     //js模块加载
     var $api = require('api');
-    var vue = require('vue');
+    var Vue = require('vue');
     var tab = require('aui_tap');
 
-    //定义一个类
-    function Index() {
-        this.init();
-        this.initUI();
-        this.initRoutes();
-        this.initEvents();
-    }
-
-    /**
-     * @description 变量管理
-     */
-    Index.prototype.init = function () {
-        var self = this;
-
-    };
-
-    /**
-     * @description 页面首次加载
-     */
-    Index.prototype.initUI = function () {
-        var self = this;
-
-        $api.fixStatusBar(document.querySelector("#jHead"));
-
-    };
-
-    /**
-     * @description 路由管理
-     */
-    Index.prototype.initRoutes = function () {
-        var self = this;
-
-    };
-
-    /**
-     * @description 事件管理
-     */
-    Index.prototype.initEvents = function () {
-        var self = this;
-
-    };
-
-    new Index();
+    new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!'
+        },
+        beforeCreate: function () {
+            $api.fixStatusBar(document.querySelector("#jHead"));
+        },
+        methods:function () {
+            
+        }
+    });
 });
